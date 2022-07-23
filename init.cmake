8ed1f6f2705c1fb871ed803b768d6ae9198f789d
Ingrater <code@benjamin-thaut.de>
2022-07-23 22:32:54 +0200

    On linux get assimp from the OS package manager (#1)

--- init.cmake
+++ init.cmake
@@ -4,9 +4,7 @@ mark_as_advanced(FORCE EZ_3RDPARTY_ASSIMP_SUPPORT)
 
 macro(ez_requires_assimp)
 
-	ez_requires_windows()
-	if (EZ_CMAKE_PLATFORM_WINDOWS_UWP)
-		return()
-	endif()
+	ez_requires(EZ_3RDPARTY_ASSIMP_SUPPORT)
+	ez_requires_one_of(EZ_CMAKE_PLATFORM_WINDOWS_DESKTOP EZ_CMAKE_PLATFORM_LINUX)
 
 endmacro()
