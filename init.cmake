b7fbbd04562d39a630451274f9e189676f7aea34
Jan Krassnigg <Jan@Krassnigg.de>
2022-08-17 18:06:58 +0200

    Custom build assimp

--- init.cmake
+++ init.cmake
@@ -1,12 +1,10 @@
-### Global variable whether to enable Advanced Docking System support
-set (EZ_3RDPARTY_ASSIMP_SUPPORT ON CACHE BOOL "Whether to add support for the asset import library (assimp).")
+### Assimp
+set (EZ_3RDPARTY_ASSIMP_SUPPORT ON CACHE BOOL "Whether to add support for the asset importer.")
 mark_as_advanced(FORCE EZ_3RDPARTY_ASSIMP_SUPPORT)
 
 macro(ez_requires_assimp)
-
-	ez_requires_windows()
-	if (EZ_CMAKE_PLATFORM_WINDOWS_UWP)
-		return()
-	endif()
+	
+	ez_requires(EZ_3RDPARTY_ASSIMP_SUPPORT)
+	ez_requires_one_of(EZ_CMAKE_PLATFORM_LINUX EZ_CMAKE_PLATFORM_WINDOWS_DESKTOP)
 
 endmacro()
