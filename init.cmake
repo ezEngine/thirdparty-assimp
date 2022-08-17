e2a2586617451f5a4f0d356e62e1d0d899507542
Jan Krassnigg <Jan@Krassnigg.de>
2022-08-17 18:47:16 +0200

    Optionally enable support for Draco compression

--- init.cmake
+++ init.cmake
@@ -2,6 +2,9 @@
 set (EZ_3RDPARTY_ASSIMP_SUPPORT ON CACHE BOOL "Whether to add support for the asset importer.")
 mark_as_advanced(FORCE EZ_3RDPARTY_ASSIMP_SUPPORT)
 
+set (EZ_3RDPARTY_ASSIMP_DRACO_SUPPORT OFF CACHE BOOL "Whether to include Draco mesh compression support in AssImp.")
+mark_as_advanced(FORCE EZ_3RDPARTY_ASSIMP_DRACO_SUPPORT)
+
 macro(ez_requires_assimp)
 	
 	ez_requires(EZ_3RDPARTY_ASSIMP_SUPPORT)
