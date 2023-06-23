19c781a1dcacfbd203fbde46afd264814979dcd1
C-Core <clemenskern@gmail.com>
2023-06-23 12:14:39 +0200

    5.2.5 update + hotfix for broken join vertex postprocessor (#3)

--- init.cmake
+++ init.cmake
@@ -1,10 +1,13 @@
-### Global variable whether to enable Advanced Docking System support
-set (EZ_3RDPARTY_ASSIMP_SUPPORT ON CACHE BOOL "Whether to add support for the asset import library (assimp).")
+### Assimp
+set (EZ_3RDPARTY_ASSIMP_SUPPORT ON CACHE BOOL "Whether to add support for the asset importer.")
 mark_as_advanced(FORCE EZ_3RDPARTY_ASSIMP_SUPPORT)
 
-macro(ez_requires_assimp)
+set (EZ_3RDPARTY_ASSIMP_DRACO_SUPPORT OFF CACHE BOOL "Whether to include Draco mesh compression support in AssImp.")
+mark_as_advanced(FORCE EZ_3RDPARTY_ASSIMP_DRACO_SUPPORT)
 
+macro(ez_requires_assimp)
+	
 	ez_requires(EZ_3RDPARTY_ASSIMP_SUPPORT)
-	ez_requires_one_of(EZ_CMAKE_PLATFORM_WINDOWS_DESKTOP EZ_CMAKE_PLATFORM_LINUX)
+	ez_requires_one_of(EZ_CMAKE_PLATFORM_LINUX EZ_CMAKE_PLATFORM_WINDOWS_DESKTOP)
 
 endmacro()
