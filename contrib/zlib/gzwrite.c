b5df25ec08fe2b837fb8e1fd01c4308822460c27
Benjamin Thaut <code@benjamin-thaut.de>
2024-08-12 12:36:31 +0200

    Avoid using windows specific _open,_read,_write,_close functions

--- contrib/zlib/gzwrite.c
+++ contrib/zlib/gzwrite.c
@@ -86,7 +86,7 @@ local int gz_comp(state, flush)
     if (state->direct) {
         while (strm->avail_in) {
             put = strm->avail_in > max ? max : strm->avail_in;
-            writ = _write(state->fd, strm->next_in, put);
+            writ = write(state->fd, strm->next_in, put);
             if (writ < 0) {
                 gz_error(state, Z_ERRNO, zstrerror());
                 return -1;
@@ -116,7 +116,7 @@ local int gz_comp(state, flush)
             while (strm->next_out > state->x.next) {
                 put = strm->next_out - state->x.next > (int)max ? max :
                       (unsigned)(strm->next_out - state->x.next);
-                writ = _write(state->fd, state->x.next, put);
+                writ = write(state->fd, state->x.next, put);
                 if (writ < 0) {
                     gz_error(state, Z_ERRNO, zstrerror());
                     return -1;
@@ -670,7 +670,7 @@ int ZEXPORT gzclose_w(file)
     }
     gz_error(state, Z_OK, NULL);
     free(state->path);
-    if (_close(state->fd) == -1)
+    if (close(state->fd) == -1)
         ret = Z_ERRNO;
     free(state);
     return ret;
